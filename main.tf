9e59ddad2db92ef483a067e81b2685bbbd7c4a68
Yusuf Kanchwala <30405568+kanchwala-yusuf@users.noreply.github.com>
2021-02-10 21:35:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "demo-s3" {
   bucket = "${var.s3_bucket_prefix}-terraformcloud"
   versioning {
     enabled = false
-    mfa_delete = false
+    mfa_delete = true
   }
 }
 
