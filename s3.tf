cb4283a2b49adaf9ad2d38d66530772745344321
Accurics <admin@accurics.com>
2021-04-28 17:52:47 +0530

    Auto Remediation By Accurics

--- s3.tf
+++ s3.tf
@@ -1,7 +1,41 @@
 resource "aws_s3_bucket" "demo-example" {
   bucket = "demoexample-1"
   versioning {
-    enabled = false
-    mfa_delete = false
+    enabled    = true
+    mfa_delete = true
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "demo-examplePolicy" {
+  bucket = "${aws_s3_bucket.demo-example.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "demo-example-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            "arn:aws:iam::##acount_id##:role/##role_name##",
+            "arn:aws:iam::##acount_id##:user/##user_name##"
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.demo-example.id}/*"
+    }
+  ]
+}
+POLICY
 }
