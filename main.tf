cd0c15b59757ba205ec9b18112bf73db10f569ca
Accurics <admin@accurics.com>
2021-10-25 20:27:01 +0530

    Auto Remediation by Accurics, file: main.tf

--- main.tf
+++ main.tf
@@ -5,11 +5,36 @@ provider "aws" {
 resource "aws_s3_bucket" "demo-s3" {
   bucket = "${var.s3_bucket_prefix}-terraformcloud"
   versioning {
-    enabled = false
-    mfa_delete = false
+    enabled    = true
+    mfa_delete = true
   }
 }
 
 variable "s3_bucket_prefix" {
   default = "sample_prefix_test20"
+}
+
+resource "aws_s3_bucket_policy" "demo-s3Policy" {
+  bucket = "${aws_s3_bucket.demo-s3.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "demo-s3-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            <aws_policy_role_arn>
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.demo-s3.id}/*"
+    }
+  ]
+}
+POLICY
 }
